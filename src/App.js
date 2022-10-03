0acba656de92208bda106add8b4dcfc44ce9073c
katya1504 <katya150491@gmail.ru>
2022-10-03 12:33:38 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { data } from './data'; 
+import { coffee } from './coffee'; 
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [cake, setCake] = useState(data);
+const [showText, setShowText] = useState(false);
+const [cafe, setCafe] = useState(0);
+const {imagetwoo}= coffee[cafe];
+
+const removeCake = (id) =>{
+  console.log(id)
+  let newCake = cake.filter((gift) => gift.id !== id);
+  setCake(newCake)
 }
+
+
+const showTextClick = (item) => {
+  item.showMore = !item.showMore
+  setShowText(!showText)
+}
+
+const previos = () =>{
+  setCafe((cafe =>{
+    cafe --;
+    if(cafe <0){
+    return coffee.length -1;
+    }
+    return cafe;
+  }))
+}
+
+const next = () =>{
+setCafe((cafe =>{
+  cafe ++;
+  if(cafe>coffee.length -1){
+    cafe=0;
+  }
+  return cafe;
+}
+))
+
+}
+
+  return (
+    <div>
+<div className="container">
+<h1>Топ лучших десертов</h1>
+</div>
+
+{cake.map((item => {
+  const{id, title, image, description, showMore} =item
+
+
+return(<div key={id}>
+<div className="container">
+      <h2>{id} - {title}</h2>
+    </div>
+ 
+<div className="container">
+      <img src={image} width='400px' height='250px' alt='image'/>
+    </div>
+
+    <div className="container">
+      <p className='text'>{showMore ? description : description.substring(0,150)+'....'}
+      <button className='btn' onClick={() => showTextClick(item)}>{showMore ? 'Свернуть' : 'Продожить'}</button></p>
+    </div>
+
+    <div className="container">
+<button className='btntwoo' onClick={() => removeCake([id])}>Закрыть</button>
+      </div>
+    </div>   
+)
+}))}
+
+<div className="container">
+<button onClick={() => setCake([])} className='thwoo'>Закрыть все</button>
+      </div>
+
+
+<div className='container'>
+<img className='imagetwoo' src={imagetwoo} width='600px' alt='foto'/>
+</div>
+
+<div className='container'>
+  <button  className='btntwoo' onClick={previos}>Назад</button>
+  <button  className='btntwoo' onClick={next}>Вперед</button>
+</div>
+</div>
+)
+}
+
+
 
 export default App;
---
+++ src/coffee.js
@@ -0,0 +1,30 @@
+export const coffee =[
+
+{
+    imagetwoo: ' https://img.freepik.com/free-photo/cup-of-coffee-and-eclair-on-a-wooden-table-closeup_169016-21339.jpg?w=996&t=st=1664543060~exp=1664543660~hmac=434cc28755d8a62c91d2fa5aff3b26a902e07b63c77f1cc0863e18917a83178a' ,
+},
+
+{
+    imagetwoo: ' https://img.freepik.com/free-photo/the-barista-working-on-steam-espresso-coffee-machine-in-a-coffee-shop_613910-20704.jpg?w=996&t=st=1664543099~exp=1664543699~hmac=2bee11d3aa70c4455b17527564387a35a588a73357b4d8bc8d87ad7471c9cbf8' ,
+},
+
+{
+    imagetwoo: ' https://img.freepik.com/free-photo/parisian-coffee-shop-style-with-tasty-coffee-served-on-wooden-table-coffee-shop-design_482257-18283.jpg?w=996&t=st=1664543139~exp=1664543739~hmac=13eab97e1e26a4fc8dc3c07d67c2874b1fba962b97a46dd86066b875360bc6a1' ,
+},
+
+{
+    imagetwoo: ' https://img.freepik.com/free-photo/interior-shot-of-a-cafe-with-chairs-near-the-bar-with-wooden-tables_181624-1669.jpg?w=996&t=st=1664543232~exp=1664543832~hmac=d5d8bca1d0bb3f9a89b28b5fce00abfd1f6bdad6317b2a20a9ab35f116c8890b' ,
+},
+
+{
+    imagetwoo: ' https://img.freepik.com/free-photo/close-up-of-sugar-dusting-on-baked-cinnamon-buns-over-the-chopping-board_23-2148161627.jpg?w=996&t=st=1664543407~exp=1664544007~hmac=bb874d8947c775c1b78c1e2f9673757159de72ebef9df266ec08c73f5bd3fdda' ,
+},
+
+{
+    imagetwoo: ' https://img.freepik.com/free-photo/a-woman-eats-a-piece-of-chocolate-cake-closeup_169016-23704.jpg?w=996&t=st=1664543368~exp=1664543968~hmac=b815d00aba8b1f16733562b6179b5e8c2a3c93e68908ddcbfb38d7e6fa862ee9' ,
+},
+
+{
+    imagetwoo: ' https://img.freepik.com/free-photo/brother-with-sister-in-cafe-choose-a-dessert-from-shop-window_627829-217.jpg?w=996&t=st=1664543320~exp=1664543920~hmac=d3b7d52ab663cf551ef76ebbbe9c732dc011f79003966e942b2c25470489c4ec' ,
+}
+]
